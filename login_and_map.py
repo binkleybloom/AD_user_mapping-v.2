6dc667f8f20bf163478bb6f3716ec2a5c2742e60
Tim Schutt <taschutt@syr.edu>
2015-05-26 14:58:47 -0400

    Added output to syslog and clarified messages.

--- login_and_map.py
+++ login_and_map.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import os, shutil, pwd, grp, subprocess
+import os, shutil, pwd, grp, subprocess, syslog
 
 empty = True
 moved = False
@@ -12,7 +12,7 @@ folders = ['/Desktop']
 
 if 'AD' in curgroup:
 
-    print "AD Mapping Script: User is network defined."
+    syslog.syslog("AD Mapping Script: User is network defined.")
 
     for folder in folders:
         curpath = homedir + folder
@@ -27,25 +27,23 @@ if 'AD' in curgroup:
                     empty = False
 
             if empty:
-                print "current path:" + curpath
-                print "AD Mapping Script: Directory is empty - deleting and redirecting."
+                syslog.syslog("AD Mapping Script: Directory is empty - deleting and redirecting.")
                 shutil.rmtree(curpath)
                 os.symlink("/Volumes/" + os.getlogin() + "$" + folder, curpath)
                 moved = True
             else:
-                print "current path:" + curpath
-                print "AD Mapping Script: Directory is not empty - copying to /Users/Shared/" + os.getlogin() + folder + " and redirecting userdir to network home"
+                syslog.syslog("AD Mapping Script: Directory is not empty - copying to /Users/Shared/" + os.getlogin() + folder + " and redirecting userdir to network home")
                 shutil.move(curpath, "/Users/Shared/" + os.getlogin() + folder)
                 os.symlink("/Volumes/" + os.getlogin() + "$" + folder, curpath)
                 moved = True
-                
+
         else:
-            print "AD Mapping Script: " + curpath + " is already linked"
+            syslog.syslog("AD Mapping Script: " + curpath + " is already linked")
 
     if moved:
         subprocess.call(['/usr/bin/killall', 'Finder'])
 
 else:
-    print "AD Mapping Script: User " + os.getlogin() + " is local - not redirecting folders."
+    syslog.syslog("AD Mapping Script: User " + os.getlogin() + " is local - not redirecting folders.")
 
 subprocess.call(['/usr/bin/rm', '/private/tmp/edu.syr.cas.adusermapping.runatload'])
